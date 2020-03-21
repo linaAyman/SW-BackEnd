81040815372040a3ed666137922b681507e7b9ba
Nancy_hassan <Nancy.hassan1998@gmail.com>
2020-03-21 17:27:25 +0200

    monogoose signin/up

--- routerApi.js
+++
@@ -1,7 +0,0 @@
-const apiRouter = require('express').Router()
-const userController = require('./controllers/userControllers')
-
-
-apiRouter.post('/login', userController.apiLogin)
-
-module.exports = apiRouter
