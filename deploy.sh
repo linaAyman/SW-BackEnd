2757ccf79273d860dcd27d41a0c7811f02fe8a86
linaAyman <45544470+linaAyman@users.noreply.github.com>
2020-03-23 21:43:10 +0200

    Add files via upload

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+ 
+#!/usr/bin/env bash
+
+set -xe
+
+ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i testaws.pem ubuntu@ec2-3-17-150-160.us-east-2.compute.amazonaws.com << EOF
+
+git clone https://github.com/linaAyman/SW-BackEnd.git
+npm install
+npm install mongodb
+npm install mongoose
+npm install nodemon
+npm install joi
+npm install validator
+npm run watch
+
+EOF
