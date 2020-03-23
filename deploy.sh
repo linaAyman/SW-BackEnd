4da064d371fae4131869fb240e0ed81982374ea2
linaAyman <45544470+linaAyman@users.noreply.github.com>
2020-03-23 22:02:22 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ set -xe
 ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i testaws.pem ubuntu@ec2-3-17-150-160.us-east-2.compute.amazonaws.com << EOF
 
 git clone https://github.com/linaAyman/SW-BackEnd.git
+cd SW-BackEnd
 npm install
 npm install mongodb
 npm install mongoose
