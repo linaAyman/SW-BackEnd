70b2896121e5683c6b187342d133bd034c8d53c5
alaa-beep <alaa98med@yahoo.com>
2020-03-25 13:56:05 +0200

    added playlist

---
+++ Desktop/SW-BackEnd-master1/deploy.sh
@@ -0,0 +1,19 @@
+ 
+#!/usr/bin/env bash
+
+set -xe
+
+ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i testaws.pem ubuntu@ec2-3-17-150-160.us-east-2.compute.amazonaws.com << EOF
+
+git clone https://github.com/linaAyman/SW-BackEnd.git
+cd SW-BackEnd
+npm install
+npm install mongodb
+npm install mongoose
+npm install nodemon
+npm install joi
+npm install validator
+sudo pm2 start app.js --watch
+exit
+
+EOF
