aae97c97095d37544173f97329928fb14e878ac6
linaAyman <45544470+linaAyman@users.noreply.github.com>
2020-03-25 15:08:21 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ set -xe
 
 ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" -i testaws.pem ubuntu@ec2-3-17-150-160.us-east-2.compute.amazonaws.com << EOF
 
+rm -rf SW-BackEnd
 git clone https://github.com/linaAyman/SW-BackEnd.git
 cd SW-BackEnd
 npm install
