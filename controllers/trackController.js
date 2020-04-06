07af6e5bc33e22904e536dff9486fcb1f042cac7
SalmaFarghaly <59613960+SalmaFarghaly@users.noreply.github.com>
2020-04-06 17:43:27 +0200

    Update trackController.js

--- controllers/trackController.js
+++ controllers/trackController.js
@@ -47,6 +47,6 @@ exports.dislikeSong=async function(req,res){
         
 
           await YourLikedSongs.updateOne({user:decoded._id},{$pull:{'tracks':{_id:tracksTemp}}});
-          return res.status(200).json({message :'OK'})
+          return res.status(200).json({message :'Deleted Successfully'})
     }
 }
