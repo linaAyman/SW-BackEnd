052ecfbe0de114d192fa1f1ba4a74334c12bbb31
SalmaFarghaly <59613960+SalmaFarghaly@users.noreply.github.com>
2020-04-07 11:37:12 +0200

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -5,10 +5,13 @@ const homeController = require('../controllers/homeController');
 const checkAuth=require('../middleware/checkAuth')
 
 router.get('/',homeController.getHome);
-router.get('/Category/:name',homeController.seeAllCategory);
+router.get('/:name',homeController.seeAll);
+
+//router.get('/',homeController.getHome);
+//router.get('/Category/:name',homeController.seeAllCategory);
 /*get/allPopularPlaylists
 get/newReleases*/
-router.get('/allPopularPlaylists',homeController.getMostPopular);
+//router.get('/allPopularPlaylists',homeController.getMostPopular);
 ///router.get('/newReleases',homeController.getNewReleases);
 
 module.exports = router;
