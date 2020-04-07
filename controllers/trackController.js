54b046dae6d08774cc8f890f02c1a2ddc1e215a2
SalmaFarghaly <59613960+SalmaFarghaly@users.noreply.github.com>
2020-04-07 10:52:08 +0200

    Update trackController.js

--- controllers/trackController.js
+++ controllers/trackController.js
@@ -18,8 +18,52 @@ exports.createLikedSongs =async function (userId)
     yourLikedSongs.save()
 
 }
- //-----------------------------Like a track-------------------------//   
+//------------------Like a track---------------------//
 exports.likeSong=async function(req,res){
+
+    let trackToLike=req.body.id;
+    if(!trackToLike) return res.status(404).send({ message: "trackId haven't been sent in the request" })
+   
+    const token = req.headers.authorization.split(" ")[1];
+    if(token){  
+
+          const decoded = jwt.decode(token);
+          let tracksTemp=await Track.findOne({id:req.body.id},{trackId:'_id'})
+          console.log(tracksTemp)
+          await YourLikedSongs.findOneAndUpdate({ user:decoded._id},{$addToSet:{'tracks':tracksTemp._id}});
+          return res.status(201).json({"message" :'OK'})
+    }
+}
+//----------------------Remove track from your liked songs---------------//
+exports.dislikeSong=async function(req,res){
+
+    
+    if(!req.body.id) return res.status(404).send({ msg: "trackId haven't been sent in the request" })
+    console.log(req.body.id)
+    const token = req.headers.authorization.split(" ")[1];
+    if(token){   
+          const decoded = jwt.decode(token);
+          let tracksTemp=await Track.findOne({id:req.body.id},{trackId:'_id'})
+        
+          console.log(tracksTemp)  
+          await YourLikedSongs.updateOne({user:decoded._id},{$pull:{tracks:tracksTemp._id}});
+          return res.status(200).json({"message" :'Deleted Successfully'})
+    }
+}
+
+//----------------------Get Liked Song Library--------------//////
+exports.getlikedSong=async function(req,res){
+
+    const token = req.headers.authorization.split(" ")[1];
+    if(token){   
+          const decoded = jwt.decode(token);
+          let tracksTemp=await YourLikedSongs.findOne({user:decoded._id},{'tracks':1,'_id':0}).populate('tracks','name image id -_id')
+          return res.status(200).json({tracksTemp})
+    }
+}
+
+ //-----------------------------Like a track-------------------------//   
+/*exports.likeSong=async function(req,res){
 
     
     if(!req.body.id) return res.status(404).send({ message: "trackId haven't been sent in the request" })
@@ -49,4 +93,4 @@ exports.dislikeSong=async function(req,res){
           await YourLikedSongs.updateOne({user:decoded._id},{$pull:{'tracks':{_id:tracksTemp}}});
           return res.status(200).json({"message" :'Deleted Successfully'})
     }
-}
+}*/
