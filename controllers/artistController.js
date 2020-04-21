c62cd09ff02b3379fc67e90c73327afe3a45b092
Arwa <marina-arwa@hotmail.com>
2020-04-22 01:27:51 +0200

    " Updated Get an artist"

--- controllers/artistController.js
+++ controllers/artistController.js
@@ -18,21 +18,9 @@ const { Artist}=require('../models/Artist')
  * @returns {jsonObject} - Object of type artist
  */
 exports.getArtist = async function (req,res){
-       /**
-         * parsing the request to get artist id from it 
-         * @member query
-         * @member temp
-         */
+       
         let temp = req.params.id;
-          /**
-         * awaits finded data by request from database
-         * @member {artist}
-         * @type {Artist}
-         */
         let artist = await Artist.find({id: temp},{'id':1,'name':1,'image':1});
-        /**
-         * if the artist exists in database, return it
-         */
         if (artist){
         return res.send(artist);
         } 
