3e77869aabf46085f9560e4de18afb7a5a20f63b
nancy9taya <nancy.hassan1998@gmail.com>
2020-06-09 21:54:40 +0200

    tests

---
+++ test/unit test-trackController.js
@@ -0,0 +1,44 @@
+var expect = require('chai').expect;
+var request = require('request');
+const dotenv = require('dotenv');
+const config = require('config');
+dotenv.config();
+const trackController = require("../controllers/trackControllers");
+
+
+describe("validate TrackSchema ", function () {
+    it('valid TrackSchema Test#1', function () {
+      let body = {
+        genre: 'classic',
+        name: 'wa7shteny',
+        artist: 'Amr Diab',
+        music: 'Uploads/Passenger Let Her Go.mp3',
+        image: 'Images/FB_IMG_1560785471157.jpg'
+      }
+      let result = trackController.validateSong(body)
+      expect(result).to.validate;
+    });
+  
+    it('notvalid TrackSchema Test#2', function () {
+      let body = {
+        genre: 'classic',
+        name: 'wa7shteny',
+        artist: 'Amr Diab',
+       music: 'Uploads/Passenger Let Her Go.mp3',
+        image: ''
+      }
+      let result = trackController.validateSong(body)
+      expect(result).to.have.an.error;
+    });
+    it('notvalid TrackSchema Test#3', function () {
+      let body = {
+        genre: 'classic',
+        name: 'wa7shteny',
+        artist: '1234',
+        music: 'Uploads/Passenger Let Her Go.mp3',
+        image: 'Images/FB_IMG_1560785471157.jpg'
+      }
+      let result = trackController.validateSong(body)
+      expect(result).to.have.an.error;
+    });
+  });
