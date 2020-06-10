f593a9dead96402393df1dcbfb508c61b73dbe8b
Salma Farghaly <SalmaFarghaly131@gmail.com>
2020-06-10 04:37:54 +0200

    Search&LibraryTests

--- test/test-searchController.js
+++ test/test-searchController.js
@@ -18,7 +18,6 @@ it('Search Feature Test#1', function() {
        if(body)
        {
         var reqBody =body.toString('utf8');
-        ///console.log(reqBody)
         reqBody = JSON.parse(reqBody);
         expect(reqBody.trackResult[0].name).to.equal('a dream is a wish your heart makes');
         }
@@ -65,7 +64,7 @@ it('Search Feature Test#3 / Artist Search test', function() {
 it('save Recent Search of type Track #4', function(done) {
   const options = {
       method:'POST',
-      url:process.env.tempurl+'/search?id=3JOF9NzQVkUXtCcJbEQuAb&type=track',
+      url:process.env.tempurl+'/search?id=3JOF9NzQVkUXtCcJbEQuAb&type=Track',
       headers: {
         'Authorization': process.env.userToken
       }
@@ -74,7 +73,7 @@ it('save Recent Search of type Track #4', function(done) {
     if(body){
       var reqBody =body.toString('utf8');
       reqBody = JSON.parse(reqBody);
-      expect(body.message).to.equal("OK");
+      expect(reqBody.message).to.equal("OK");
      }
      done();
   });
@@ -83,7 +82,7 @@ it('save Recent Search of type Track #4', function(done) {
 it('save Recent Search of type Playlist #5', function(done) {
   const options = {
       method:'POST',
-      url:process.env.tempurl+'/search?id=4qrimFUz8KFC8W6WrDiDnh&type=playlist',
+      url:process.env.tempurl+'/search?id=4qrimFUz8KFC8W6WrDiDnh&type=Playlist',
       headers: {
         'Authorization': process.env.userToken
       }
@@ -92,11 +91,107 @@ it('save Recent Search of type Playlist #5', function(done) {
     if(body){
       var reqBody =body.toString('utf8');
       reqBody = JSON.parse(reqBody);
-      expect(body.message).to.equal("OK");
+      expect(reqBody.message).to.equal("OK");
      }
      done();
   });
 });
+
+it('save Recent Search of type Artist #6', function(done) {
+  const options = {
+      method:'POST',
+      url:process.env.tempurl+'/search?id=19gmxCK2V3jLMi5fDYyKtS&type=Artist',
+      headers: {
+        'Authorization': process.env.userToken
+      }
+    };
+  request(options, function(error, response, body) {
+    if(body){
+      var reqBody =body.toString('utf8');
+      reqBody = JSON.parse(reqBody);
+      expect(reqBody.message).to.equal("OK");
+     }
+     done();
+  });
+});
+
+it('get Recent Search for a user it should have the above 3 items #7', function(done) {
+  const options = {
+      method:'GET',
+      url:process.env.tempurl+'/search/recent-search',
+      headers: {
+        'Authorization': process.env.userToken
+      }
+    };
+  request(options, function(error, response, body) {
+    if(body){
+      var reqBody =body.toString('utf8');
+      reqBody = JSON.parse(reqBody);
+      reqBody.recentSearch[0].id.should.equal('19gmxCK2V3jLMi5fDYyKtS');
+      reqBody.recentSearch[1].id.should.equal('4qrimFUz8KFC8W6WrDiDnh');
+      reqBody.recentSearch[2].id.should.equal('3JOF9NzQVkUXtCcJbEQuAb');
+      reqBody.count.should.equal(3);
+     }
+     done();
+  });
+});
+
+
+it('delete item from Recent Search for a user #8', function(done) {
+  const options = {
+      method:'DELETE',
+      url:process.env.tempurl+'/search?id=4qrimFUz8KFC8W6WrDiDnh&type=Playlist',
+      headers: {
+        'Authorization': process.env.userToken
+      }
+    };
+  request(options, function(error, response, body) {
+    if(body){
+      var reqBody =body.toString('utf8');
+      reqBody = JSON.parse(reqBody);
+      expect(reqBody.message).to.equal("OK");
+     }
+     done();
+  });
+});
+
+it('get Recent Search for a user to make sure previous item is deleted #9', function(done) {
+  const options = {
+      method:'GET',
+      url:process.env.tempurl+'/search/recent-search',
+      headers: {
+        'Authorization': process.env.userToken
+      }
+    };
+  request(options, function(error, response, body) {
+    if(body){
+      var reqBody =body.toString('utf8');
+      reqBody = JSON.parse(reqBody);
+      reqBody.recentSearch[0].id.should.equal('19gmxCK2V3jLMi5fDYyKtS');
+      reqBody.recentSearch[1].id.should.equal('3JOF9NzQVkUXtCcJbEQuAb');
+      reqBody.count.should.equal(2);
+     }
+     done();
+  });
+});
+it('Delete all search history for user#10', function(done) {
+  const options = {
+      method:'DELETE',
+      url:process.env.tempurl+'/search/all',
+      headers: {
+        'Authorization': process.env.userToken
+      }
+    };
+  request(options, function(error, response, body) {
+    if(body){
+      var reqBody =body.toString('utf8');
+      reqBody = JSON.parse(reqBody);
+      expect(reqBody.message).to.equal("OK");
+     }
+     done();
+  });
+});
+
 
 
 
